7766b2b95fc91017f2874c622f07c79c6785421c
koito19960406 <61777450+koito19960406@users.noreply.github.com>
2023-08-28 09:06:41 +0800

    changing test.py

--- src/models/test_model.py
+++ src/models/test_model.py
@@ -65,9 +65,38 @@ def create_fid_csv(root_folder):
     with open(root_folder / "fid.tex", 'w') as f:
         f.write(latex_table)
 
-if __name__ == '__main__':
-    # Make a list of drives
-    drives = ['E:/', 'F:/', "/Volumes/ExFAT/"]
+def pool_images(data_path, model_path, output_path):
+    """Pool images of real gsv panorama, real gsv perspective, real mapillary, fake mapillary (by models) by image id
+
+    Args:
+        data_path (_type_): _description_
+        model_path (_type_): _description_
+        output_path (_type_): _description_
+    """
+    # get a list of ids in one of the folders
+    processed_path = data_path / 'processed'
+    # list directories in processed_path and check if the name contains 'cyclegan' (because they have an easier structure)
+    model_data_folder = [x for x in processed_path.iterdir() if x.is_dir() and "cyclegan" in x.name][0]
+    # get a list of ids in the folder testA
+    img_id_list = [str(x.stem).replace(".jpg", "") for x in (model_data_folder / 'testA').iterdir()]
+    # loop through processed_path again and make a dictionary of all the images in all model folders
+    for model_name in processed_path.iterdir():
+        if model_name.is_dir():
+            # get a list of all the images in the model folder
+            model_img_list = [x for x in model_name.iterdir() if x.is_file() and x.suffix == '.jpg']
+            # loop through the list and copy the images to the output folder
+            for img in model_img_list:
+                # get the id of the image
+                img_id = str(img.stem).replace(".jpg", "")
+                # check if the id is in the list of ids
+                if img_id in img_id_list:
+                    # copy the image to the output folder
+                    shutil.copy2(img, output_path / f"{model_name.name}_{img.name}")
+    # loop through the id list and copy the images to the model folder
+    for img_id in img_id_list:
+        # get real images of GSV panorama and perspective and Mapillary images
+        
+    
 
     # Set relative paths
     script_path_relative = Path("road_shoulder_gan/src/models/pytorch-CycleGAN-and-pix2pix/test.py")
